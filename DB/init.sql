9579f36d868133ff3bd262c203a305387ba858ad
Alberto Provenzano <provi.al2106@gmail.com>
2024-11-14 23:02:19 +0100

    Migliorato il meccanismo di inizializzazione del db

---
+++ DB/init.sql
@@ -0,0 +1,25 @@
+-- init.sql
+
+-- Crea la tabella 'Users'
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    ticker VARCHAR(50) NOT NULL
+);
+
+-- Crea la tabella 'Data' con una foreign key che fa riferimento a 'Users'
+CREATE TABLE IF NOT EXISTS Data (
+    timestamp DATETIME NOT NULL,
+    id INT NOT NULL,
+    ticker VARCHAR(50) NOT NULL,
+    valore_euro DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (timestamp, id, ticker),
+    FOREIGN KEY (id) REFERENCES Users(id)
+);
+
+
+INSERT INTO Users (email, ticker) VALUES
+('alberto@example.com', 'AAPL'),
+('giuseppe@example.com', 'GOOG'),
+('maria@example.com', 'MSFT'),
+('luigi@example.com', 'AMZN');
