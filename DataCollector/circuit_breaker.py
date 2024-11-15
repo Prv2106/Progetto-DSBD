99214bfce261e93683f6c0a30899b0e6d818199c
Alberto Provenzano <provi.al2106@gmail.com>
2024-11-15 20:51:17 +0100

    implementato circuit breaker e aggiornato il data collector

---
+++ DataCollector/circuit_breaker.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import time
+import threading
+import pytz
+
+tz = pytz.timezone('Europe/Rome') 
+
+class CircuitBreaker:
+
+    # Metodo costruttore
+    def __init__(self, f_threshold = 3, r_timeout = 20, e_exception = Exception):
+        
+        #definizione degli attributi di istanza:
+
+        self.state = "CLOSED" # inizialmente lo stato del circuit breaker è closed -> tutte le richieste passano
+        self.f_threshold = f_threshold # soglia di errore oltre la quale si passa allo stato OPEN
+        self.r_timeout = r_timeout # tempo necessario da apettare prima di poter passare allo stato HALF_OPEN
+        self.last_failure_time = None # istante di tempo durante il quale è venuto l'ultimo faailure
+        self.f_count = 0 # contatore degli errori
+        self.lock = threading.Lock()  # Creazione di un lock per la gestione di una sezione critica
+        self.e_exception = e_exception
+
+
+    # Metodo che si occupa di eseguire la richiesta in base allo stato del circuito
+    def call(self,func,*args,**kargs):
+        with self.lock: # Acquisizione del lock
+
+            if self.state == "OPEN":
+                time_since_failure = time.time() - self.last_failure_time
+                if time_since_failure >= self.r_timeout:
+                    self.state = "HALF_OPEN"
+                else:
+                    # Il circuito è ancora aperto
+                    raise CircuitBreakerOpenException("Il circuito è aperto... Chiamata rifiutata.")
+            try:
+                # tentativo di esecuzione della richiesta
+                result = func(*args,**kargs)
+
+            except self.e_exception as e: # se la richiesta fallisce
+                
+                self.last_failure_time = time.time()
+                self.f_count +=1
+                if self.f_count >= self.f_threshold:
+                    self.state = "OPEN"
+                raise e # rilancia al chiamante l'eccezione generata
+                
+            
+            # se la richiesta viene eseguita con successo 
+            else:
+                if self.state == "HALF_OPEN":
+                    self.f_count = 0
+                    self.state = "CLOSED"
+
+                print(f"Circuit Breaker: richiesta eseguita con successo ({datetime.now(tz)})")
+                return result
+
+class CircuitBreakerOpenException(Exception): # classe che estende Exception
+    pass
--- DataCollector/data_collector.py
+++ DataCollector/data_collector.py
@@ -3,8 +3,10 @@ from datetime import datetime
 import time
 import yfinance as yf
 import pytz
+from circuit_breaker import CircuitBreakerOpenException, CircuitBreaker
 
-tz = pytz.timezone('Europe/Rome')  
+tz = pytz.timezone('Europe/Rome') 
+
 
 
 # Configurazione per il database
@@ -20,6 +22,8 @@ insert_query = """
     INSERT INTO Data (timestamp, id, ticker, valore_euro)
     VALUES (%s, %s, %s, %s);
 """
+
+# Query che per ogni id e ticker restituisce quello più vecchio
 delete_old_query = """
     DELETE D 
     FROM Data D
@@ -31,75 +35,94 @@ delete_old_query = """
     WHERE D.id = %s AND D.ticker = %s;
 """
 
-def fetch_from_user_db():
-    conn = pymysql.connect(**db_config)
+
+circuit_breaker = CircuitBreaker(f_threshold = 4, r_timeout= 30)
+maximum_occurrences = 10
+
+def fetch_users_from_db(conn):
+    # Usa la connessione passata come parametro
     try:
         with conn.cursor() as cursor:
             cursor.execute("SELECT id, ticker FROM Users;")            
-            result = cursor.fetchall()
+            result = cursor.fetchall() 
 
-            if not result:
+            if not result:  # condizione di lista vuota
                 return [], []
 
-            # otteniamo 2 liste separate a partire da una lista di tuple
-            ids, tickers = zip(*result)
+            # Otteniamo 2 liste separate a partire da una lista di tuple
+            ids, tickers = zip(*result)  # unpacking
             return ids, tickers
-    finally:
-        conn.close()
+    except Exception as e:
+        print(f"Errore durante il recupero degli utenti: {e}")
+        return [], []
+
 
 def fetch_yfinance_data(ticker):
     stock = yf.Ticker(ticker)
+
+    # Otteniamo i dati storici dell'oggetto ticker per 1 giorno
     data = stock.history(period="1d")
     
     if data.empty:
         raise ValueError(f"No data found for ticker {ticker}")
     
+    """
+    Viene estratto il valore del prezzo di chiusura (Close) del primo (e unico) giorno nel DataFrame data. 
+    La funzione .iloc[0] restituisce il primo valore nella colonna "Close", che è il prezzo di chiusura per il giorno richiesto.
+    """
     closing_price_usd = data['Close'].iloc[0]
 
-    # cambio USD-EUR (puoi considerare di aggiornarlo dinamicamente)
-    usd_to_eur_rate = 0.9  # valore fisso; considera l'aggiornamento dinamico se necessario
+    # cambio USD-EUR 
+    usd_to_eur_rate = 0.9  
     closing_price_eur = closing_price_usd * usd_to_eur_rate
     return closing_price_eur
 
 def data_collector():
-    print("Connessione..")
     time.sleep(5)
-    # Connessione al database
+    # Connessione al database (una sola volta)
     conn = pymysql.connect(**db_config)
-    ids, tickers = fetch_from_user_db()
-    print("Utenti recuperati")
-    if not ids or not tickers:
-        print("Nessun dato trovato nella tabella Users.")
-        return
-
     try:
-        with conn.cursor() as cursor:
+        # Otteniamo gli utenti (id, ticker) dalla tabella Users
+        ids, tickers = fetch_users_from_db(conn)  # Passiamo la connessione
+        print("data_collector.py: Utenti recuperati con successo!")
+
+        if not ids or not tickers:
+            print("data_collector.py: Nessun dato trovato nella tabella Users.")
+            return
+
+       
+        with conn.cursor() as cursor: # crea un oggetto cursore (che è utilizzato per eseguire le query nel database)
+            # Cicliamo per ogni coppia (id, ticker)
             for id, ticker in zip(ids, tickers):
                 try:
-                    price_in_eur = fetch_yfinance_data(ticker)
-                except ValueError as e:
-                    print(e)
-                    continue
+                    # Richiede i dati al Circuit Breaker
+                    price_in_eur = circuit_breaker.call(fetch_yfinance_data, ticker)
+                except CircuitBreakerOpenException as e:
+                    print(f"{e}")  # Eccezione sollevata quando il Circuit Breaker è nello stato "OPEN"
+                    continue  # Continuiamo con il prossimo ticker
 
-                timestamp = datetime.now(tz)  # Ottiene il timestamp corrente
+                # Otteniamo il timestamp corrente
+                timestamp = datetime.now(tz)
 
-                # Inserimento nel database
+                # Inseriamo i dati nel database
                 cursor.execute(insert_query, (timestamp, id, ticker, price_in_eur))
-                
+
                 # Contiamo le occorrenze presenti per la coppia (id, ticker)
                 cursor.execute("SELECT COUNT(*) FROM Data WHERE id = %s AND ticker = %s", (id, ticker))
                 count = cursor.fetchone()[0]
-                
-                if count > 30:  # Se ci sono più di 30 occorrenze, eliminiamo la più vecchia
+
+                # Se ci sono più di maximum_occurrences, eliminiamo la più vecchia
+                if count > maximum_occurrences:
                     cursor.execute(delete_old_query, (id, ticker))
-            
-            # Conferma delle modifiche
+                print(f"data_collector.py: Operazione per utente -> {id} e ticker -> {ticker} conclusa con successo alle ore: {datetime.now(tz)}")
+
+            # Confermiamo tutte le modifiche nel database
             conn.commit()
 
     finally:
+        # Chiudiamo la connessione al database
         conn.close()
-        print(f"Operazione conclusa con successo alle ore: {datetime.now(tz)}")
 
-# Esegui il DataCollector 
+
 if __name__ == "__main__": 
     data_collector()
