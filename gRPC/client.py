2322545771f54bc6f7a1515d0be132128e2e3750
Alberto Provenzano <provi.al2106@gmail.com>
2024-11-16 20:08:53 +0100

    implementazione AMO per register user

--- gRPC/client.py
+++ gRPC/client.py
@@ -1,18 +1,41 @@
 import grpc
 import usermanagement_pb2
 import usermanagement_pb2_grpc
+import random
+
+
 
 def register_user(stub):
     # Crea una richiesta per la registrazione utente (modifica i dettagli come desiderato)
     email = input("Inserisci la tua email: ")
     ticker = input("Inserisci il ticker del tuo investimento: ")
 
-    request = usermanagement_pb2.UserRegisterRequest(email=email, ticker=ticker)
+    metadata = [
+        ('user_id', email),
+        ('request_id',"register_req") 
+    ]
+
+    max_attempts = 20
+
+    # Meccanismo di "timeout & retry"
+    for attempt in range(max_attempts):
+        request = usermanagement_pb2.UserRegisterRequest(email=email, ticker=ticker)
+        try:
+            # Invoca il metodo RegisterUser e ottieni la risposta
+            response = stub.RegisterUser(request, metadata = metadata,timeout = 2)
+            print(f"Esito: {response.success}, Messaggio: {response.message}")
+            return
+        
+        except grpc.RpcError as error:
+            if error.code() == grpc.StatusCode.DEADLINE_EXCEEDED: # se è scaduto il timeout
+                print(f"Timeout superato, tentativo {attempt + 1} di {max_attempts}")
+                continue  # Prova un altro tentativo
+            
+            elif error.code() in {grpc.StatusCode.UNAVAILABLE, grpc.StatusCode.CANCELLED}:
+                print(f"Errore: {error}")
+                break  # Esce dal ciclo poiché l'errore non è recuperabile
     
-    # Invoca il metodo RegisterUser e ottieni la risposta
-    response = stub.RegisterUser(request)
-    
-    print(f"Success: {response.success}, Message: {response.message}")
+    print("Non è stato possibile completare la richiesta")
     
 
 def show_menu():
@@ -20,23 +43,30 @@ def show_menu():
     print("1. Registrati")
     print("2. Esci")
 
+
+
 def run():
-    # Connessione al server gRPC in ascolto sulla porta 50051
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = usermanagement_pb2_grpc.UserServiceStub(channel)
+       # Connessione al server gRPC in ascolto sulla porta 50051
+    with grpc.insecure_channel('localhost:50051') as channel:
+        # Crea uno stub per il servizio UserService
+        stub = usermanagement_pb2_grpc.UserServiceStub(channel)
 
-    while True:
-        # Mostra il menu e ottieni la scelta dell'utente
-        show_menu()
-        choice = input("Scegli un'opzione (1 o 2): ")
+        while True:
+            # Mostra il menu e ottieni la scelta dell'utente
+            show_menu()
+            choice = input("Scegli un'opzione (1 o 2): ")
 
-        if choice == "1":
-            register_user(stub)
-        elif choice == "2":
-            print("Uscita...")
-            break
-        else:
-            print("Opzione non valida, riprova.")
+            if choice == "1":
+                register_user(stub)
+            elif choice == "2":
+                print("Uscita...")
+                break
+            else:
+                print("Opzione non valida, riprova.")
+
+
+
+
 
 if __name__ == '__main__':
     run()
--- gRPC/server.py
+++ gRPC/server.py
@@ -2,6 +2,17 @@ import grpc
 from concurrent import futures
 import usermanagement_pb2
 import usermanagement_pb2_grpc
+from threading import Lock
+import pymysql
+import logging
+import time
+
+# Configurazione del logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+request_cache = {}  # dizionario di dizionari per consentire il controllo non soltanto sulla richiesta ma anche sull'utente
+cache_lock = Lock()
 
 # Configurazione per il database
 db_config = {
@@ -11,27 +22,80 @@ db_config = {
     "database": "DSBD_progetto"
 }
 
+# QUERYs
+register_user_query = """
+    INSERT INTO Users (email, ticker)
+    VALUES (%s,%s);
+"""
 
+# Implementazione del servizio UserService che estende UserServiceServicer generato da protoc
+class UserServiceServicer(usermanagement_pb2_grpc.UserServiceServicer): 
 
-# Implementazione del servizio UserService
-class UserServiceServicer(usermanagement_pb2_grpc.UserServiceServicer):
+    # Servizio per la registrazione degli utenti
     def RegisterUser(self, request, context):
-        # Logica di registrazione utente
-        print(f"Registrazione utente: {request.email}, Ticker: {request.ticker}")
-        # TODO: query database
-        response = usermanagement_pb2.UserResponse(success=True, message="User registered successfully by server gRPC!")
         
+        # Estrazione dei metadati dal contesto
+        metadata = dict(context.invocation_metadata())
+        user_id = metadata.get('user_id', "unknown")
+        request_id = metadata.get('request_id', "unknown")
+
+        logger.info(f"Metadati ricevuti: UserId -> {user_id}, RequestID -> {request_id}")
+
+        with cache_lock:
+            # Log della cache per il debug
+            logger.info("Contenuto della cache (chiave -> valore):")
+            for request_id_key, user_cache in request_cache.items():
+                for user_id_key, processed in user_cache.items():
+                    logger.info(f"RequestID: {request_id_key}, UserID: {user_id_key}, Processato: {processed}")
+            
+            # Verifichiamo se la richiesta era stata già processata
+            if request_id in request_cache:
+                if user_id in request_cache[request_id]:
+                    logger.info(f"Richiesta già elaborata per l'utente {user_id}")
+                    return usermanagement_pb2.UserResponse(success=True, message="Utente già registrato")
+
+        try:
+            # Logica di registrazione utente
+            logger.info(f"Registrazione utente: {request.email}, Ticker: {request.ticker}")
+            conn = pymysql.connect(**db_config)
+            with conn.cursor() as cursor:
+                cursor.execute(register_user_query, (request.email, request.ticker))
+                conn.commit()
+
+            response = usermanagement_pb2.UserResponse(success=True, message="Utente registrato con successo!")
+
+            # Memorizzazione della risposta nella cache
+            with cache_lock:
+                if request_id not in request_cache:
+                    request_cache[request_id] = {}
+                request_cache[request_id][user_id] = True
+
+        except pymysql.MySQLError as err:
+            logger.error(f"Errore durante l'inserimento nel database: {err}")
+            response = usermanagement_pb2.UserResponse(success=False, message=f"Errore database: {err}")
+        
+        finally:
+            # Chiudiamo la connessione al database
+            conn.close()
+            
+        # test per il timeout
+        # time.sleep(3)
         return response
 
+
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    usermanagement_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
-    
-    # Ascolta sulla porta 50051
-    server.add_insecure_port('[::]:50051')
-    print("Server in ascolto sulla porta 50051...")
-    server.start()
-    server.wait_for_termination()
+    try:
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        usermanagement_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
+
+        # Ascolta sulla porta 50051
+        server.add_insecure_port('[::]:50051')
+        logger.info("Server in ascolto sulla porta 50051...")
+        server.start()
+        server.wait_for_termination()
+    except Exception as e:
+        logger.error(f"Errore durante l'avvio del server: {e}")
+
 
 if __name__ == '__main__':
     serve()
