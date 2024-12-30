4ddc59a6aaf320bfea0983ed82b001c38c1ce2d2
Giuseppe Leocata <peppeleocata@gmail.com>
2024-12-30 19:49:40 +0100

    Migrazione a Kubernetes RIUSCITA!

--- DB/db_config.py
+++ DB/db_config.py
@@ -1,6 +1,6 @@
 # Configurazione per il database
 db_config = {
-    "host": "mysql-container",
+    "host": "mysql-container", # 'mysql-container' è il nome del Service associato al Pod mysql
     "user": "alberto_giuseppe",
     "password": "progetto",
     "database": "DSBD_progetto"
